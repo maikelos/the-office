64fc6acf9db4097baa47c751b06016ed7d005356
Michal Wesolowski <maikelos272@gmail.com>
2022-09-16 14:19:05 +0100

    Added docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -13,7 +17,10 @@ author = 'Michal'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/index.rst
+++ docs/index.rst
@@ -10,6 +10,8 @@ Welcome to The Office's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+   modules
+
 Workspace
 ---------
 
