410225256242013f4d26b7851e117e72ef65d05c
Michal Wesolowski <maikelos272@gmail.com>
2022-09-16 14:11:56 +0100

    added docstrings

--- employee.py
+++ employee.py
@@ -5,16 +5,40 @@ JOB_TITLES = ["SALES_REP", "REGIONAL_MANAGER", "MANAGER", "HR", "ACCOUNTING"]
 
 class Employee:
     def __init__(self, name: str, job_title: str):
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         self.name = name
         self.job_title = job_title
         self.employee_id = uuid.uuid4()
 
     def update_contact_info(self, name: str) -> str:
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         past_name = self.name
         self.name = name
         return past_name
 
     def promote_employee(self, job_title):
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         if job_title not in JOB_TITLES:
             raise ValueError(
                 f"Invalid job title {job_title}, expected one of {JOB_TITLES}"
--- office.py
+++ office.py
@@ -7,19 +7,60 @@ from employee import Employee
 class TheOffice:
 
     def __init__(self, regional_manager: str, employees: Set[Employee], location: str):
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         self.regional_manager = regional_manager
         self.employees = employees
         self.location = location
         self.sales = defaultdict(int)
 
     def __str__(self) -> str:
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         return f"Welcome to The Office! The boss here is {self.regional_manager}."
 
     def record_sale(self, employee: Employee) -> None:
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
+
         self.sales[employee] += 1
 
     def get_employee_sales(self, employee: Employee) -> int:
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         return self.sales[employee]
 
     def _fire_employee(self, employee: Employee) -> None:
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         self.employees.remove(employee)
